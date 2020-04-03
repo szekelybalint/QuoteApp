bb5825ac53658fa7ba2e6fded7d760c8afa2645e
Bálint Székely <szekely.balint@autsoft.hu>
2020-04-03 13:55:14 +0200

    initial commit

---
+++ app/src/main/java/aut/bme/hu/quoteapp/MainActivity.java
@@ -0,0 +1,14 @@
+package aut.bme.hu.quoteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
